e415fa740396d8e58571922f292eaa3edc19fd4d
Hannu Viinikainen <hannu.viinikainen@vincit.fi>
2023-10-05 14:47:54 +0300

    Add auth configuration file

---
+++ src/authConfig.ts
@@ -0,0 +1,31 @@
+export const b2cPolicies = {
+    names: {
+        signUpSignIn: "B2C_1_ReadingDiarySignUpSignIn"
+    },
+    authorities: {
+        signUpSignIn: {
+            authority: "https://piggycorp.b2clogin.com/piggycorp.onmicrosoft.com/B2C_1_ReadingDiarySignUpSignIn",
+        }
+    },
+    authorityDomain: "piggycorp.b2clogin.com"
+};
+
+
+export const msalConfig: Configuration = {
+    auth: {
+        clientId: '151ee949-0e8b-4632-9bd9-851da2f80748',
+        authority: b2cPolicies.authorities.signUpSignIn.authority,
+        knownAuthorities: [b2cPolicies.authorityDomain],
+        redirectUri: '/',
+    }
+}
+
+export const protectedResources = {
+    readingDiaryApi: {
+        endpoint: "http://localhost:5000/",
+        scopes: {
+            read: ["https://piggycorp.onmicrosoft.com/reading-diary-api/reading-diary.read"],
+            write: ["https://piggycorp.onmicrosoft.com/reading-diary-api/reading-diary.write"]
+        }
+    }
+}
